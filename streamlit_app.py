4fa570f5aab2a01729c8128e99f5560e2b1e79ef
Vijaykumar Deshmukh <67310914+viijaytdeshmukh@users.noreply.github.com>
2023-03-18 15:21:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
-import streamlit 
-streamlit.title('IP Trip Prediction')
-streamlit.header('Can Umesh travel ladakh?')
-streamlit.text('yes but orange inc should be give him leave then their are chances')
-                 
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
