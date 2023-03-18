cda94a30a8a1d80c89c38c0f4b5fdfd13fcd6243
Vijaykumar Deshmukh <67310914+viijaytdeshmukh@users.noreply.github.com>
2023-03-18 15:25:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.text('🍇 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🌯 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🍗 Hard-Boiled Free-Range Egg')
 
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
