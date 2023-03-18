c1433f5ae5fce0e145797580893dfc0cd32b7016
Vijaykumar Deshmukh <67310914+viijaytdeshmukh@users.noreply.github.com>
2023-03-18 15:14:53 +0530

    Update streamlit_app.py
    error recovery

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit 
 streamlit.title('IP Trip Prediction')
 straemlit.header('Can Umesh travel ladakh?')
-streamlit.text('yes but orange inc should be give him leave then their are chances)
+streamlit.text('yes but orange inc should be give him leave then their are chances')
                  
