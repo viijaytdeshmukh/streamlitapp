725bae06883426a6c487ba5687691e25ba4d4d62
Vijaykumar Deshmukh <67310914+viijaytdeshmukh@users.noreply.github.com>
2023-03-18 15:14:22 +0530

    Update streamlit_app.py
    adding simple header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit 
 streamlit.title('IP Trip Prediction')
+straemlit.header('Can Umesh travel ladakh?')
+streamlit.text('yes but orange inc should be give him leave then their are chances)
+                 
